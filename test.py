d5eae263a5926ffeade88f8e0fbd1be5e47ddea8
yangbo.zhou <yangbo.zhou@jina.ai>
2023-01-09 09:17:27 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,25 @@
+import openai
+# Replace YOUR_API_KEY with your OpenAI API key
+openai.api_key = "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"
+
+# Set the model and prompt
+model_engine = "text-davinci-003"
+prompt = "写一篇AI芯片论文"
+
+# Set the maximum number of tokens to generate in the response
+max_tokens = 128
+
+# Generate a response
+completion = openai.Completion.create(
+    engine=model_engine,
+    prompt=prompt,
+    max_tokens=1024,
+    temperature=0.5,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+)
+
+# Print the response
+print(completion.choices[0].text)
+
